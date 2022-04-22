52ebd12805767988d1ec41dbce5f61df02f85ddc
yunifuchioka <fuchioka@cs.ubc.ca>
2022-04-22 16:11:40 -0700

    friction constraints matrix

--- main.py
+++ main.py
@@ -31,12 +31,32 @@ def calc_A_dyn_t(l1tx, l1ty, l2tx, l2ty):
     return A_dyn_t
 
 
+def calc_A_fric_t():
+    A_fric_t = sp.lil_matrix((6, dim_x))
+    A_fric_t[0, 6] = -1
+    A_fric_t[0, 7] = 1
+    A_fric_t[1, 6] = 1
+    A_fric_t[1, 7] = 1
+    A_fric_t[2, 8] = -1
+    A_fric_t[2, 9] = 1
+    A_fric_t[3, 8] = 1
+    A_fric_t[3, 9] = 1
+    A_fric_t[4, 7] = 1
+    A_fric_t[5, 9] = 1
+
+    return A_fric_t
+
+
 if __name__ == "__main__":
     A_dyn_t = calc_A_dyn_t(1, 2, 3, 4)
     l_dyn_t = np.array([0, 0, 0, m * g * dt, 0, 0])
     u_dyn_t = l_dyn_t
 
-    print(A_dyn_t.toarray())
+    A_fric_t = calc_A_fric_t()
+    l_fric_t = np.zeros(6)
+    u_fric_t = np.full(6, np.inf)
+
+    print(A_fric_t.toarray())
 
     import ipdb
 
